b8e76afd5274e5bd957c0f4b8dbd21b30dbcc4f1
its-sainath <sebitwar1997@gmail.com>
2023-01-08 19:24:17 +0530

    Using Custom JPA Username password validation

--- src/main/java/com/therealsainath/configs/SecurityConfigs.java
+++ src/main/java/com/therealsainath/configs/SecurityConfigs.java
@@ -2,22 +2,11 @@ package com.therealsainath.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfigs {
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        var userDetailsManager = new InMemoryUserDetailsManager();
-        var userDetails = User.builder().username("test").password("test").roles("read").build();
-        userDetailsManager.createUser(userDetails);
-        return userDetailsManager;
-    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
